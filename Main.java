09270c28b7db349705498078c5afab018a8e049a
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-13 13:38:00 +0000

    Finished method mark calculator

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+// this line creates a procedure method with the method name exam grade and the paramater is the mark
+  public static void examGrade(int grade){
+    // if and else if statements that check where the users mark falls under
+    if(grade >= 80){
+      System.out.println("A");
+    } else if(grade <= 79 && grade >= 70){
+      System.out.println("B");
+    } else if(grade <= 69 && grade >= 60){
+      System.out.println("C");
+    } else if(grade <= 59 && grade >= 50){
+      System.out.println("D");
+    } else if(grade < 50){
+      System.out.println("F");
+    }
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  examGrade(80);   
   }
 }
